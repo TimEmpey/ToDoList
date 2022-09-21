c43f5fafdd9b81fcad98c1adf2ed58b75233393b
Tim Empey <timothyempey@gmail.com>
2022-09-21 14:36:36 -0700

    add error message for viewing empty list
    Co-authored-by: Harold Mesa <curiousmockingbird@users.noreply.github.com>

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -8,11 +8,43 @@ namespace ToDoList
   {
     public static void Main()
     {
-      Console.WriteLine("Welcome to the To Do List");
-      // Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
-      string description = Console.ReadLine();
-      Item newItem = new Item(description);
-      Console.WriteLine(newItem.Description);
+      bool userWantsToExit = false;
+
+      while(!userWantsToExit)
+      {
+        Console.WriteLine("-----------------------------------------------------------------------------");
+        Console.WriteLine("To Do List");
+        Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View/Exit)");
+        Console.WriteLine("-----------------------------------------------------------------------------");
+        string choice = Console.ReadLine().ToLower();
+        Console.WriteLine("-----------------------------------------------------------------------------");
+
+        if (choice == "view") {
+          // bool isEmpty = !Item.Any();
+          if (Item.GetAll().Count == 0) {
+            Console.WriteLine("Your list doesn't have any items yet!");
+          } else {
+            foreach (Item thisItem in Item.GetAll()) 
+            {
+              Console.WriteLine(thisItem.Description);
+            }
+          }
+          
+        } else if (choice == "add") {
+          Console.WriteLine("Add something to your list:");
+          string description = Console.ReadLine();
+          Item newItem = new Item(description);
+          foreach (Item thisItem in Item.GetAll()) 
+          {
+            Console.WriteLine("-----------------------------------------------------------------------------");
+            Console.WriteLine(thisItem.Description);
+          }
+        } else if (choice == "exit") {
+          userWantsToExit = true;
+        } else {
+          Console.WriteLine("That is not an option");
+        }
+      }
     }
   }
 }
